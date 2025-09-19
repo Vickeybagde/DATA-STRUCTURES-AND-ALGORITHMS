6a10ea46ceb2c3f0253dc3314e7625cb84d1ea4c
Vickeybagde <vickeybagde799@gmail.com>
2025-09-19 21:39:22 +0530

    palindrome

---
+++ Strings/Palindrome1.java
@@ -0,0 +1,23 @@
+public class Palindrome1 {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+        return true; // All characters matched
+    }
+
+    public static void main(String[] args) {
+        String str = "noon";
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+}
