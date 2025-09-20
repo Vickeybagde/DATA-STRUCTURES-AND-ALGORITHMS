f05b9dc7729d4f5afe177f221c7291a495675db6
Vickeybagde <vickeybagde799@gmail.com>
2025-09-20 15:06:09 +0530

    DSA

--- Strings/compare.java
+++ Strings/compare.java
@@ -18,6 +18,16 @@ public class compare {
 
         // ye 1 (str1.equals (str3) ) koi string kisi dusre string ke equal hai ya nahi wo check karti hai
 
+        /*
+         * why string are immutable?
+         * humne isme dekha ki jab hum new key word banate hai aur same string pass karte hai to java bata hai ki String equla nahi hai 
+         * is concept ko interning kahte hai  hamare pass compter ke andat do tarah ki  memory hoti hai 
+         * stack and heap naam ki jab hum likhte hai str1 = "vickey"  to str1 jake stack memory me store ho jata hai aur "vickey" heap memory me
+         * jab hum koi dusri string jaise str2 = "vickey" banate hai aur same string likte hai usme bhi aur jab hum in dono string ko check karte hai equal hai ya nahi to 
+         * to ye dono String heap memory me Store same string ko point karte hai 
+         * aur jab hum koi naya  object banate hai aur usme bhi same string pass karte hai to ye 1 nayi string ban jati hai
+         */
+
 
         if(str1.equals(str3)){
             System.out.println("Strings are equal");
